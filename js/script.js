99e55697e8c137f5ccc9d690bdb3419ac24415ed
OzodbekMahmaraimov <ozodbekmahmaraimov07@gmail.com>
2023-12-15 18:01:24 +0500

    lider practice 1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let counterElement = document.getElementById('.count');
+    let currentNumber = 0;
+
+    function updateCounter() {
+        counterElement.textContent = currentNumber;
+        currentNumber++;
+
+        if (currentNumber > 70) {
+            clearInterval(intervalId);
+        }
+    }
+
+    let intervalId = setInterval(updateCounter, 100); // Adjust the interval as needed
+
+});
+
+let navItem = document.querySelectorAll('.nav-link');
+const navAnimation = () => {
+    navItem.forEach((item) => {
+        item.style.cssText = 'animation: nav-animation 2s ease-in-out'
+    })
+}
